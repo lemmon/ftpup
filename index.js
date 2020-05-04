e4f719a9df6536c57a05c78f02f2741886ab0464
Lemmon <lemmon@users.noreply.github.com>
2020-05-04 17:38:00 +0200

    ftp -> basic-ftp

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const path = require('path')
-const glob = require('./src/glob')
-const ftp = require('./src/ftp')
+const ftp = require('basic-ftp')
+const glob = require('./lib/glob')
+
+const client = new ftp.Client()
 
 module.exports = async (opts) => {
   try {
@@ -9,56 +11,51 @@ module.exports = async (opts) => {
     if (opts.test) {
       console.log('> TEST MODE')
     }
-    await ftp.connect({
+    await client.access({
       host: opts.host,
-      username: opts.username,
+      user: opts.username,
       password: opts.password,
-      directory: opts.remoteDir,
     })
     console.log('> connected')
-    const dir = path.join(process.cwd(), opts.localDir || '.')
+    // directories
+    const localDir = path.join(process.cwd(), opts.localDir || '.')
+    const remoteDir = path.join('/', opts.remoteDir || '.')
     // upload files
     const files = await glob({
-      cwd: dir,
+      cwd: localDir,
       ignore: opts.ignore,
     })
     for (const f of files) {
-      const ff = path.join(dir, f)
+      const ff = path.join(localDir, f)
       const st = fs.lstatSync(ff)
       if (st.isFile()) {
         console.log('+', f)
-        if (!opts.test) await ftp.upload(ff, f)
+        if (!opts.test) {
+          await client.uploadFrom(ff, path.basename(f))
+        }
       } else if (st.isDirectory()) {
-        if (!opts.test) await ftp.mkdir(f)
+        if (!opts.test) {
+          await client.cd(remoteDir)
+          await client.ensureDir(f)
+        }
       }
     }
     // purge directory
     for (const dir of opts.purge) {
-      await rmdir(opts, dir, false).catch(err => {
-        if (err.code !== 450) throw err
-      })
+      console.log('-', path.join(dir, '*'))
+      if (!opts.test) {
+        await client.cd(path.join(remoteDir, dir)).catch(err => {
+          if (err.code === 550) return
+          throw err
+        })
+        await client.clearWorkingDir()
+      }
     }
     // close connection
-    await ftp.close()
+    await client.close()
     console.log('> done')
   } catch (err) {
-    await ftp.close()
+    await client.close()
     throw err
-  }
-}
-
-async function rmdir(opts, dir, includeSelf = true) {
-  const list = (await ftp.list(dir)).filter(curr => !curr.name.match(/^\.+$/))
-  for (const item of list) {
-    const file = path.join(dir, item.name)
-    if (item.type !== 'd') {
-      console.log('-', file)
-      if (!opts.test) await ftp.rm(file)
-    } else {
-      await rmdir(opts, file)
-    }
-  }
-  if (includeSelf) {
-    if (!opts.test) await ftp.rmdir(dir)
   }
 }

5bc9dc48cd61efd10058c385fecc1a3ea29aff66
Lemmon <lemmon@users.noreply.github.com>
2020-05-13 21:20:32 +0200

    remove remote files, dirs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const stream = require('stream')
 const ftp = require('basic-ftp')
 const glob = require('./lib/glob')
 
@@ -8,7 +9,9 @@ const client = new ftp.Client()
 module.exports = async (opts) => {
   try {
     // state
-    const state = {}
+    const state = {
+      local: new Map,
+    }
     // open connection
     if (opts.test) {
       console.log('> TEST MODE')
@@ -22,7 +25,17 @@ module.exports = async (opts) => {
     // directories
     const localDir = path.join(process.cwd(), opts.localDir || '.')
     const remoteDir = path.join('/', opts.remoteDir || '.')
-    await client.cd(remoteDir)
+    // remote state
+    try {
+      await client.cd(remoteDir)
+      const ws = new WS
+      await client.downloadTo(ws, '.ftpup')
+      state.remote = new Map(ws.getJSON())
+      state.toRemove = new Set(state.remote.keys())
+    } catch (e) {
+      state.remote = new Map()
+      state.toRemove = new Set()
+    }
     // upload files
     const files = await glob({
       cwd: localDir,
@@ -31,27 +44,63 @@ module.exports = async (opts) => {
     for (const file of files) {
       const ff = path.join(localDir, file)
       const st = fs.lstatSync(ff)
+      state.local.set(file, st.mtimeMs)
+      state.toRemove.delete(file)
       if (st.isFile()) {
-        console.log('+', file)
-        if (!opts.test) {
-          const dir = path.dirname(file)
-          if (dir !== state.dir) {
-            await client.cd(path.join(remoteDir, dir))
-            state.dir = dir
-          }
-          await client.uploadFrom(ff, path.basename(file))
+        if (st.mtimeMs <= state.remote.get(file)) continue
+        console.log('+', 'F', file)
+        if (opts.test) continue
+        const dir = path.dirname(file)
+        if (dir !== state.dir) {
+          await client.cd(path.join(remoteDir, dir))
+          state.dir = dir
         }
+        await client.uploadFrom(ff, path.basename(file))
       } else if (st.isDirectory()) {
-        if (!opts.test) {
-          await client.cd(remoteDir)
-          await client.ensureDir(file)
-          state.dir = file
-        }
+        if (state.remote.has(file)) continue
+        console.log('+', 'D', file)
+        if (opts.test) continue
+        await client.cd(remoteDir)
+        await client.ensureDir(file)
+        state.dir = file
       }
+    }
+    // remove files
+    if (!opts.test) {
+      await client.cd(remoteDir)
+    }
+    for (const file of state.toRemove) {
+      console.log('-', 'F', file)
+      if (!opts.test) {
+        await client.remove(file).catch(err => {
+          if (err.code === 550) return
+          throw err
+        })
+      }
+    }
+    // remove directories
+    if (!opts.test) {
+      await client.cd(remoteDir)
+    }
+    const toRemove = new Set(
+      Array.from(state.toRemove)
+        .sort((a, b) => b.split('/').length - a.split('/').length)
+    )
+    for (const dir of toRemove) {
+      if (opts.test || dir === '.') continue
+      if (state.local.has(dir)) continue
+      const list = await client.list(dir).catch(err => {
+        if (err.code === 450 || err.code === 550) return
+        throw err
+      })
+      if (list === undefined || list.length) continue
+      console.log('-', 'D', dir)
+      await client.removeDir(path.join(remoteDir, dir))
+      toRemove.add(dir)
     }
     // purge directory
     for (const dir of opts.purge) {
-      console.log('-', path.join(dir, '*'))
+      console.log('-', '*', path.join(dir, '*'))
       if (!opts.test) {
         await client.cd(path.join(remoteDir, dir)).then(() => (
           client.clearWorkingDir()
@@ -61,11 +110,32 @@ module.exports = async (opts) => {
         })
       }
     }
+    if (!opts.test) {
+      await client.cd(remoteDir)
+      const rs = new stream.Readable
+      rs.push(JSON.stringify(Array.from(state.local)))
+      rs.push(null)
+      await client.uploadFrom(rs, '.ftpup')
+    }
     // close connection
     await client.close()
     console.log('> done')
   } catch (err) {
     await client.close()
     throw err
+  }
+}
+
+class WS extends stream.Writable {
+  str = ''
+  _write(chunk, encoding, cb) {
+    this.str += chunk.toString()
+    cb()
+  }
+  getString() {
+    return this.str
+  }
+  getJSON() {
+    return JSON.parse(this.str)
   }
 }
